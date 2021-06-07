1335ce2cd60dc424f74f89ee46dbcb05f9b8af0f
Victor Gaugler <victor.gaugler@hotmail.fr>
2021-06-07 13:04:23 +0200

    bonus  commit

--- routes/tags.js
+++ routes/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fakeTags = require('../data/tags');
-const fakePosts = require('../data/posts');
+const allPosts = require('../data/posts');
 
 const router = express.Router();
 
@@ -22,11 +22,15 @@ router.get('/:tagId', (req, res) => {
   return res.json(foundTag);
 });
 
-// Get a single post
 router.get('/:tagId/posts', (req, res) => {
   const tagId = Number(req.params.tagId);
-  // Keep only comments whose post_id matches the postId parameter
-  const foundPost = fakePosts.filter((post) => post.tag_ids === tagId);
-  res.json(foundPost);
+  const foundTag = allPosts.filter((post) => post.tag_ids.includes(tagId));
+  if (!foundTag) {
+    return res.status(404).json({
+      error: 'Post not found',
+    });
+  }
+  return res.json(foundTag);
 });
+
 module.exports = router;
